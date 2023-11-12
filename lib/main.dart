559dc571f64654516b1744250c35270634ab1cdb
nusrat <nusrat.jahan.nira@outlook.com>
2023-11-13 00:13:00 +0600

    design modified with better version.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 const picSource =
-    'https://i.pinimg.com/originals/55/51/4d/55514dfd272080a0f6f0e2074205aa80.jpg';
+    'https://whc.unesco.org/uploads/thumbs/site_0798_0006-1000-627-20120827171714.jpg';
 const description =
-    'Mauris rhoncus dolor ut pretium lacinia. Donec a gravida dui. Duis a tellus in enim pellentesque sagittis. Sed quis nunc ornare, ullamcorper dolor ac, posuere erat. Integer efficitur rutrum tortor, non bibendum eros laoreet at. Vestibulum aliquet eros vel libero semper ultrices. Maecenas aliquam, risus vel tristique hendrerit, nisi leo dictum nibh, a feugiat eros libero ac magna. Proin vulputate orci vitae gravida suscipit. Vivamus vitae nisl vel augue varius dapibus mattis vel dui. Vestibulum venenatis faucibus blandit. Vestibulum eget mauris ut ex luctus iaculis. Nullam ac quam eu quam congue efficitur vestibulum a elit.';
-
+'''The Sundarbans mangrove forest, one of the largest such forests in the world (140,000 ha), lies on the delta of the Ganges, Brahmaputra and Meghna rivers on the Bay of Bengal. It is adjacent to the border of India’s Sundarbans World Heritage site inscribed in 1987. The site is intersected by a complex network of tidal waterways, mudflats and small islands of salt-tolerant mangrove forests, and presents an excellent example of ongoing ecological processes. The area is known for its wide range of fauna, including 260 bird species, the Bengal tiger and other threatened species such as the estuarine crocodile and the Indian python.''';
 void main() {
   runApp(const MyApp());
 }
@@ -15,109 +14,115 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            Image.network(
-              picSource,
-              height: 300,
-              width: double.infinity,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Oeschinen Lake Campground",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 14),
-                      ),
-                      SizedBox(height: 5,),
-                      Text(
-                        "Kandersteg, Switzerland",
-                        style: TextStyle(fontSize: 14, color: Colors.black38),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star,
-                        color: Colors.deepOrange,
-                      ),
-                      Text(
-                        "41",
-                        style: TextStyle(color: Colors.black87),
-                      ),
-                    ],
-                  ),
-                ],
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                picSource,
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.call,
-                        color: Colors.blue,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "CALL",
-                          style: TextStyle(color: Colors.blue),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "The Sundarbans",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
                         ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.route_outlined,
-                        color: Colors.blue,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "ROUTE",
-                          style: TextStyle(color: Colors.blue),
+                        SizedBox(height: 5,),
+                        Text(
+                          "Khulna Division, Bangladesh",
+                          style: TextStyle(fontSize: 14, color: Colors.grey),
                         ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.share,
-                        color: Colors.blue,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "SHARE",
-                          style: TextStyle(color: Colors.blue),
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                      ],
+                    ),
+                    Spacer(),
+                    Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    Text(
+                      "9.5",
+                      style: TextStyle(color: Colors.black87),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 2.0,left: 16.0,right: 16.0),
-              child: Text(description),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.call,
+                          color: Colors.blue,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "CALL",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.near_me,
+                          color: Colors.blue,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "ROUTE",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.share,
+                          color: Colors.blue,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "SHARE",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(description),
+              )
+            ],
+          ),
         ),
       ),
     );
